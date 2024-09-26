45c389e3b7e2ec8164947f9913ebe4af8be5211c
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-09-25 18:19:54 -0600

    Created card components

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { faker } from '@faker-js/faker'
+
+const fullname = faker.person.fullName()
+
+
+
+</script>
+
+
+<template>
+{{ fullname }} <br />
+
+
+
+
+</template>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainCardSingle from './MainCardSingle.vue';
+
+
+
+
+</script>
+
+<template>
+
+<MainCardSingle v-for = "card in 20"> </MainCardSingle>
+
+
+</template>
